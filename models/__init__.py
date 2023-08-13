ed0522bf772d06e4094724565acb5b38362bf1cf
Edgar <eddy8471@gmail.com>
2023-08-13 17:46:35 +0300

    STorage file
    created change in base model
    instance of storage in models

--- models/__init__.py
+++ models/__init__.py
@@ -2,5 +2,6 @@
 """ initiate FileStoarge """
 from models.engine.file_storage import FileStorage
 
+"""instance of storage"""
 storage = FileStorage()
 storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -1,34 +1,10 @@
-<<<<<<< HEAD
-#!usr/bin/python3
-
-"""Base Class Module"""
-
-import uuid
-from datetime import datetime
-from copy import deepcopy
-import models
-
-
-class BaseModel():
-    """ Base model class """
-
-    def __init__(self, *args, **kwargs):
-        """Initialize a BaseModel instance"""
-
-        if len(kwargs.items()) > 0:
-            for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
-                if key == "created_at" or key == "updated_at":
-                    self.__setattr__(key,  datetime.fromisoformat(value))
-                    continue
-                self.__setattr__(key, value)
-=======
-import uuid
-from datetime import datetime
-
+#!/usr/bin/python3
 """module that defines common attr for other classes
 """
+
+import uuid
+from datetime import datetime
+import models
 
 class BaseModel:
     """class basemodel"""
@@ -50,42 +26,11 @@ class BaseModel:
             # self.created_at = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
             # self.updated_at = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
         
->>>>>>> 0c7d4eb90c5b2a86cf17b08b59423df993dc970f
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-<<<<<<< HEAD
             models.storage.new(self)
-
-    def save(self):
-        """Update the updated_at attribute with the current datetime"""
-
-        self.updated_at = datetime.now()
-
-        models.storage.save()
-
-    def to_dict(self):
-        """
-        Return a dictionary representation of the instance
-        containing all attributes
-        """
-        new_dict = deepcopy(self.__dict__)
-        new_dict['created_at'] = self.created_at.isoformat()
-        new_dict['updated_at'] = self.updated_at.isoformat()
-        new_dict['__class__'] = self.__class__.__name__
-        return new_dict
-
-    def __str__(self):
-        """
-        Return a string representation of the instance
-        """
-        return "[{}] ({}) {}".format(
-            self.__class__.__name__,
-            self.id,
-            self.__dict__
-        )
-=======
 
     def __str__(self):
         """string rep of the class"""
@@ -94,6 +39,7 @@ class BaseModel:
     def save(self):
         """updated instance updated_at"""
         self.updated_at = datetime.now()
+        models.storage.new(self)
 
     def to_dict(self):
         """dict containing key value pairs"""
@@ -106,4 +52,4 @@ class BaseModel:
         # n_dict['updated_at'] = n_dict['updated_at'].isoformat()
 
         return n_dict
->>>>>>> 0c7d4eb90c5b2a86cf17b08b59423df993dc970f
+    
