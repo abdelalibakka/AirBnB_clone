e001b3f60424107245626f4c12b41dc5adccdd95
Edgar Yungo <eddy8471@gmail.com>
2023-08-13 20:39:03 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -30,7 +30,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            models.storage.save()
 
     def __str__(self):
         """string rep of the class"""
